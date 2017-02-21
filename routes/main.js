a119d97752183b08c644ddc2a64be0bd201a3d4e
KimJunYeop <wnsduq8282@gmail.com>
2017-02-22 04:46:21 +0900

    main page 갯수 오류 수정.

--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,11 @@ router.get(['/','/:id'], function(req, res, next) {
   var limit = 8;
   var page = req.params.id;
   var offsetCount = 0;
+  var length;
+  //첫번째 for문 길이
+  var first_for_length;
+  //두번째 for문 길이
+  var second_for_length;
 
   if(page===undefined){
     //페이지가 없다면.
@@ -24,12 +29,24 @@ router.get(['/','/:id'], function(req, res, next) {
 
   connection.query(rowsCount,function(err,rows,fiels){
     if(err) console.error("rowsCount Error : " + err);
-    //rowscount = 행의 갯수
+    console.log('rowslength :' +  rows[0].rowscount);
     pageCount = parseInt(rows[0].rowscount/limit+1);
     connection.query(sql,function(err,topics,fields){
       if(err) console.error("sql error : " + err);
-      console.log(topics);
-      res.render('main', { title: 'JunYeop' , topics : topics , pageCount : pageCount});
+      if(topics.length<4){
+        first_for_length = topics.length;
+        second_for_length = 0;
+      }else{
+        first_for_length = 4;
+        second_for_length = topics.length;
+      }
+      // if(topics.length<8){
+      //   length = topics.length;
+      // }else{
+      //   length = topics.length;
+      // }
+      console.log(topics.length);
+      res.render('main', { title: 'JunYeop' , topics : topics , pageCount : pageCount, first : first_for_length, second : second_for_length});
     });
   })
 });
